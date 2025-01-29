07edf8df8bd1841d1c524fd62ddda92b3a70767c
Nerf22117 <40220191@esmad.ipp.pt>
2025-01-29 19:10:54 +0000

    Update package.json to downgrade json-server version and enhance server.js to log server status on startup

--- server.js
+++ server.js
@@ -6,4 +6,8 @@ const middlewares = jsonServer.defaults();
 server.use(middlewares);
 server.use(router);
 
+server.listen(3000, () => {
+  console.log("JSON Server is running");
+});
+
 module.exports = server;
